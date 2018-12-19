4a80b18648ead4ed6aa28a342f64d5398b305904
MarcosPernica <marcospernica@gmail.com>
2018-12-18 21:32:38 -0300

    Corregida unidad de TDEV

--- allan.py
+++ allan.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 
 def plotTDEV(TE):
 	a = allantools.Dataset(data=TE)
-	a.compute("TDEV [s]")
+	a.compute("tdev")
 
 	plotter = allantools.Plot()
 	plotter.plot(a, errorbars=True, grid=True)
 
 	plotter.ax.set_xlabel("Tau [s]")
+	plotter.ax.set_ylabel("TDEV [s]")
 	plt.title("Varianza de Allan para NTPv4")
 	plotter.show()
