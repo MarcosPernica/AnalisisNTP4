c8c222dc8df928f5eb3235ed0d6a81219e8cbd9f
eze210 <eze210@gmail.com>
2018-12-27 23:40:05 -0300

    Updating allan function. Fixing some strings. Fixing slide window.

--- allan.py
+++ allan.py
@@ -1,14 +1,18 @@
 import allantools
 import matplotlib.pyplot as plt
+import numpy as np
 
+
+# Receives the TE in seconds and plots the Allan TDEV
 def plotTDEV(TE):
-	a = allantools.Dataset(data=TE)
-	a.compute("tdev")
+    frequency = np.diff(TE[0::64])
+    a = allantools.Dataset(data=frequency, rate=1.0/64, data_type='freq')
+    a.compute("tdev")
 
-	plotter = allantools.Plot()
-	plotter.plot(a, errorbars=True, grid=True)
+    plotter = allantools.Plot()
+    plotter.plot(a, errorbars=True, grid=True)
 
-	plotter.ax.set_xlabel("Tau [s]")
-	plotter.ax.set_ylabel("TDEV [s]")
-	plt.title("Varianza de Allan para NTPv4")
-	plotter.show()
+    plotter.ax.set_xlabel("Tau [s]")
+    plotter.ax.set_ylabel("TDEV [s]")
+    plt.title("Varianza de Allan para NTPv4")
+    plotter.show()
